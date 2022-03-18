8c3d4480e159832f1abacbd651720f79e21c4c57
Demian Ruban <rubandemian@gmail.com>
2022-03-18 18:13:01 +0100

    Make the right loop function

--- functionloop.js
+++ functionloop.js
@@ -6,10 +6,10 @@ function goldfactory(){
     }
     count = 0;
     context.clearRect(0, 0, canvas.width, canvas.height);
-    
+
     snake.x += snake.dx;
     snake.y += snake.dy;
-    
+
     if(snake.x < 0){
         snake.x = canvas.width - grid;
     }
@@ -22,9 +22,9 @@ function goldfactory(){
     else if(snake.y >= canvas.height){
         snake.y = 0
     }
-    
+
     snake.cells.unshift({x: snake.x, y: snake.y});
-    
+
     if(snake.cells.length > snake.maxCells){
         snake.cells.pop();
     }
@@ -36,16 +36,16 @@ context.fillRect(cube.x, cube.y, grid - 1, grid - 1);
 context.fillStyle = 'black';
 
 snake.cells.forEach(function (cell,index){
-    
+
     context.fillRect(cell.x, cell.y, grid - 1, grid - 1);
-    
+
     if(cell.x === cube.x && cell.y === cube.y){
         snake.maxCells++;
-        
+
         cube.x = getRandomInt(0, 25) * grid;
         cube.y = getRandomInt(0, 25) * grid;
     }
-    
+
     for (var i = index +1; i < snake.cells.length; i++){
         if(cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
             snake.x = 160;
@@ -54,10 +54,10 @@ snake.cells.forEach(function (cell,index){
             snake.maxCells = 2;
             snake.dx = 0;
             snake.dy = grid;
-            
+
             cube.x = getRandomInt(0,25)*grid;
             cube.y = getRandomInt(0,25)*grid;
         }
     }
   });
-requestAnimationFrame(loop);
+requestAnimationFrame(goldfactory);
